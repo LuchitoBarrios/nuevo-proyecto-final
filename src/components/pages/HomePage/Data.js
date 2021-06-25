5552c471440e0b4d9be87d223a444ce0bdd19f58
LuchitoBarrios <71348217+LuchitoBarrios@users.noreply.github.com>
2021-06-25 13:34:48 -0500

    nuevo proyecto final v1

---
+++ src/components/pages/HomePage/Data.js
@@ -0,0 +1,52 @@
+export const homeObjOne={
+    lightBg:false,
+    lightText:true,
+    lightTextDesc:true,
+    topLine:'Restaurante Familiar',
+    headline:'Restaurant Turístico Los Compadres',
+    description:'Tenemos la mejor propuesta gastronómica de sabores intensos, todos preparados por nuestro Chef Internacional Harrison Loayza Carpio',
+    buttonLabel:'Ordenar',
+    imgStart:'',
+    img:'../../public/images/compadres2.jpg',
+    alt:'Los Compadres'
+
+}
+export const homeObjTwo={
+    lightBg:false,
+    lightText:true,
+    lightTextDesc:true,
+    topLine:'Restaurante Familiar',
+    headline:'Restaurant Turístico Los Compadres',
+    description:'Tenemos la mejor propuesta gastronómica de sabores intensos, todos preparados por nuestro Chef Internacional Harrison Loayza Carpio',
+    buttonLabel:'Ordenar',
+    imgStart:'',
+    img:'../../public/images/compadres2.jpg',
+    alt:'Los Compadres'
+
+}
+export const homeObjThree={
+    lightBg:false,
+    lightText:true,
+    lightTextDesc:true,
+    topLine:'Restaurante Familiar',
+    headline:'Restaurant Turístico Los Compadres',
+    description:'Tenemos la mejor propuesta gastronómica de sabores intensos, todos preparados por nuestro Chef Internacional Harrison Loayza Carpio',
+    buttonLabel:'Ordenar',
+    imgStart:'',
+    img:'../../public/images/compadres2.jpg',
+    alt:'Los Compadres'
+
+}
+export const homeObjFour={
+    lightBg:false,
+    lightText:true,
+    lightTextDesc:true,
+    topLine:'Restaurante Familiar',
+    headline:'Restaurant Turístico Los Compadres',
+    description:'Tenemos la mejor propuesta gastronómica de sabores intensos, todos preparados por nuestro Chef Internacional Harrison Loayza Carpio',
+    buttonLabel:'Ordenar',
+    imgStart:'',
+    img:'../../public/images/compadres2.jpg',
+    alt:'Los Compadres'
+
+}
